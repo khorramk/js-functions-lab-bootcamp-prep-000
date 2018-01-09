4eda2d8ca4007930a16d89d5da56cb2684a461a8
Khorram Khondkar <khorramk.kbsk@gmail.com>
2018-01-09 19:14:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function happyHolidaysTo(name){
 }
 
 function holidayCountdown(holiday, days){
-  return `${days} days until ${name}`;
+  return `${days} days until ${holiday}!`;
 }
 
 function happyHolidayTo(holiday, name){
